37bf427822a2f6126c3c29208a2f0b8ba90a15db
AlyciaEXE <alicia.silva@sptech.school>
2026-08-26 22:25:58 -0300

    Arrumei o script do banco

--- AquaShield/src/database/script-tabelas.sql
+++ AquaShield/src/database/script-tabelas.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS aquashield;
 CREATE DATABASE IF NOT EXISTS aquashield;
+USE aquashield;
 
 CREATE TABLE empresa (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
@@ -14,7 +15,7 @@ nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE usuario (
-id INT PRIMARY KEY AUTO_INCREMENT,
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(50),
 email VARCHAR(50),
 senha VARCHAR(50),
@@ -24,44 +25,44 @@ CONSTRAINT fkUsuarioCargo FOREIGN KEY (fkCargo) REFERENCES cargo(idCargo),
 CONSTRAINT fkUsuarioEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
-CREATE TABLE maquinas(
+CREATE TABLE maquina(
 idMaquina INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100),
 fkEmpresa INT,
-CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa));
+CONSTRAINT fkMaquinaEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa));
 
 
-CREATE TABLE Processador(
- id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE processador(
+ idProcessador INT PRIMARY KEY AUTO_INCREMENT,
+ dtHora DATETIME DEFAULT CURRENT_TIMESTAMP,
  temperatura DECIMAL(3,1),
  frequencia DECIMAL(7,2),
- porcentagem_de_uso DECIMAL(5,2),
+ porcentagemUso DECIMAL(5,2),
  fkMaquina INT,
- fkEmpresa INT,
- CONSTRAINT fkMaquina FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
- CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa));
+ CONSTRAINT fkProcessadorMaquina FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina)
+ );
  
  
  CREATE TABLE memoria(
  idMemoria INT PRIMARY KEY AUTO_INCREMENT,
+ dtHora DATETIME DEFAULT CURRENT_TIMESTAMP,
  memoria_usada INT,
  memoria_disponivel INT,
  fkMaquina INT,
- fkEmpresa INT,
- CONSTRAINT fkMaquina FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
- CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+ CONSTRAINT fkMemoriaMaquina FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina)
+ 
  );
  
  CREATE TABLE disco(
  idDisco INT PRIMARY KEY AUTO_INCREMENT,
+ dtHora DATETIME DEFAULT CURRENT_TIMESTAMP,
  discoUso INT,
  espaçoLivre INT,
  fkMaquina INT,
- fkEmpresa INT,
- CONSTRAINT fkMaquina FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina),
- CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
+ CONSTRAINT fkDiscoMaquina FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina)
+ 
  );
  
-INSERT INTO cargo (id,nome) VALUES
+INSERT INTO cargo (idCargo,nome) VALUES
 (1,"Técnico de Automação"),
 (2,"Controlador de Sistemas De Saneamento")
